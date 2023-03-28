f429e9baab5e23c37b044188dd9c9d2bc3fd5958
nderhore <nderhore@partner.auchan.com>
2023-03-28 15:00:36 +0200

    update speaker conf

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,8 +1,14 @@
 import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-school-theme.mjs';
 
+const speakers = [
+  // comment speakers before presenting
+  'TSI',
+  'NDE'
+].map((trigram) => `speakers/${trigram}`);
+
 // One method per module
 function schoolSlides() {
-  return ['00-school/00-TITLE.md', '00-school/speaker-jef.md'];
+  return ['00-school/00-TITLE.md', '00-school/speakers/speaker-jef.md'];
 }
 
 function introSlides() {
