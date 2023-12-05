a0e3edf6ef312390012dbd32cd8fbe9b0a68ea58
Anthony Pena <anthony.pena@outlook.fr>
2023-12-05 23:25:47 +0100

    slides: better speakers slides

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,23 +1,19 @@
 import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-school-theme.mjs';
 
-const speakers = [
-  // comment speakers before presenting
-  'TSI.md',
-  'NDE.md',
-  'Participant.md'
-].map((trigram) => `00-school/speakers/${trigram}`);
-
 // One method per module
 function schoolSlides() {
-  return ['00-school/00-TITLE.md', ...speakers, "00-school/01-objectives.md"];
+  return [
+    '00-school/00-TITLE.md',
+    '00-school/speaker-sironneau_t.md',
+    '00-school/speaker-derhore_n.md',
+    '00-school/speaker-pena_a.md',
+    '00-school/Participant.md',
+    '00-school/01-objectives.md',
+  ];
 }
 
-function introSlides() {
-  return ['00-school/00-TITLE.md\''];
-}
-
-function firstPartSlides(){
-  return[
+function firstPartSlides() {
+  return [
     '01-part1/01-Introduction.md',
     '01-part1/02-module-description.md',
     '01-part1/03-dev-mode.md',
@@ -27,33 +23,34 @@ function firstPartSlides(){
     '01-part1/07-crud-api.md',
     '01-part1/08-db-configuration.md',
     '01-part1/09-test-application.md',
-  ]
+  ];
 }
 
-function secondPartSlides(){
-  return['02-part2-reactive/01-Introduction.md',
+function secondPartSlides() {
+  return [
+    '02-part2-reactive/01-Introduction.md',
     '02-part2-reactive/02-module-description.md',
     '02-part2-reactive/03-dev-mode.md',
     '02-part2-reactive/04-entity.md',
     '02-part2-reactive/05-repository.md',
-    '02-part2-reactive/06-crud-api.md'
-  ]
+    '02-part2-reactive/06-crud-api.md',
+  ];
 }
 
-function thirdPartSlides(){
-  return['03-part3-openapi/01-open-api.md']
+function thirdPartSlides() {
+  return ['03-part3-openapi/01-open-api.md'];
 }
 
-function fourthPartSlides(){
-  return['04-part4-BFF/01-bff.md']
+function fourthPartSlides() {
+  return ['04-part4-BFF/01-bff.md'];
 }
 
-function conclusion(){
-  return['05-part5-conclusion/01-conclusion.md']
+function conclusion() {
+  return ['05-part5-conclusion/01-conclusion.md'];
 }
 
-function bonusPartSlides(){
-  return['09-part-bonus/01-Introduction.md']
+function bonusPartSlides() {
+  return ['09-part-bonus/01-Introduction.md'];
 }
 
 function formation() {
